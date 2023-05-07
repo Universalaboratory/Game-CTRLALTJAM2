38f9c620d2ec5e48326393b8235fd8147d9545a7
ThicosGroove <thiagottlsilva@gmail.com>
2023-05-07 16:56:54 -0300

    player particle pooling system working

---
+++ Game-CTRLALTJAM2/Assets/Scripts/Entities/Player/ParticleSpawner.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+namespace Entities.Player
+{
+    public class ParticleSpawner : MonoBehaviour
+    {
+        public ObjectPool<ParticlePool> _pool;
+
+        private PlayerShootBehaviour _playerShootBehaviour;
+
+        private void Start()
+        {
+            _playerShootBehaviour = GetComponent<PlayerShootBehaviour>();
+            _pool = new ObjectPool<ParticlePool>(CreateParticles, OnTakeParticleFromPool, OnReturnParticleToPool, OnDestroyParticles, true, 200, 350);
+        }
+
+        private ParticlePool CreateParticles()
+        {
+            ParticlePool particles = Instantiate(_playerShootBehaviour._particlePrefab, _playerShootBehaviour._bulletSpawn.position, _playerShootBehaviour._bulletSpawn.rotation);
+            particles.SetPool(_pool);
+            return particles;
+        }
+
+        private void OnTakeParticleFromPool(ParticlePool particles)
+        {
+            particles.transform.position = _playerShootBehaviour._bulletSpawn.position;
+            particles.transform.rotation = _playerShootBehaviour._bulletSpawn.rotation;
+
+            particles.gameObject.SetActive(true);
+        }
+
+        private void OnReturnParticleToPool(ParticlePool particles)
+        {
+            particles.gameObject.SetActive(false);
+
+        }
+
+        private void OnDestroyParticles(ParticlePool particles)
+        {
+            Destroy(particles.gameObject);
+        }
+
+    }
+
+}
+
--- Game-CTRLALTJAM2/Assets/Scripts/Entities/Player/PlayerShootBehaviour.cs
+++ Game-CTRLALTJAM2/Assets/Scripts/Entities/Player/PlayerShootBehaviour.cs
@@ -3,60 +3,69 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerShootBehaviour : MonoBehaviour
+namespace Entities.Player 
 {
-    [SerializeField] private ParticleSystem _particlePrefab;
-    [SerializeField] private Transform _bulletSpawn;
-    [SerializeField] private float _fireRate;
-
-    private float timer;
-    private bool _isShooting;
-
-    private InputControl _input;
-
-    private void Awake()
+    public class PlayerShootBehaviour : MonoBehaviour
     {
-        _input = new InputControl();
-    }
+        [SerializeField] public ParticlePool _particlePrefab;
+        [SerializeField] public Transform _bulletSpawn;
+        [SerializeField] private float _fireRate;
 
-    private void OnEnable()
-    {
-        _input.Player.Shoot.performed += OnShootThisFrame;
-        _input.Player.Shoot.canceled += OnStopShooting;
-        _input.Enable();
-    }
+        private float timer;
+        private bool _isShooting;
 
-    private void OnDisable()
-    {
-        _input.Disable();
-        _input.Player.Shoot.performed -= OnShootThisFrame;
-        _input.Player.Shoot.canceled -= OnStopShooting;
-    }
+        private ParticleSpawner _particleSpawner;
+        private InputControl _input;
 
-    void Update()
-    {
-        ShootBehaviour();
-    }
 
-    private void OnShootThisFrame(InputAction.CallbackContext context) => _isShooting = true;
-
-    private void OnStopShooting(InputAction.CallbackContext context) => _isShooting = false;
-
-    private void ShootBehaviour()
-    {
-        if (!_isShooting) return;
-       
-        timer += Time.deltaTime;
-
-        float nextTimeToFire = 1 / _fireRate;
-
-        if (timer >= nextTimeToFire)
+        private void Awake()
         {
-            var bullet = Instantiate(_particlePrefab, _bulletSpawn.position, _bulletSpawn.rotation);
-            var main = bullet.main;
-            main.startSpeed = 10;
+            _input = new InputControl();
+        }
 
-            timer = 0;
-        }               
-    }        
+        private void Start()
+        {
+            _particleSpawner = GetComponent<ParticleSpawner>();
+        }
+
+        private void OnEnable()
+        {
+            _input.Player.Shoot.performed += OnShootThisFrame;
+            _input.Player.Shoot.canceled += OnStopShooting;
+            _input.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _input.Disable();
+            _input.Player.Shoot.performed -= OnShootThisFrame;
+            _input.Player.Shoot.canceled -= OnStopShooting;
+        }
+
+        void Update()
+        {
+            ShootBehaviour();
+        }
+
+        private void OnShootThisFrame(InputAction.CallbackContext context) => _isShooting = true;
+
+        private void OnStopShooting(InputAction.CallbackContext context) => _isShooting = false;
+
+        private void ShootBehaviour()
+        {
+            if (!_isShooting) return;
+
+            timer += Time.deltaTime;
+
+            float nextTimeToFire = 1 / _fireRate;
+
+            if (timer >= nextTimeToFire)
+            {
+                _particleSpawner._pool.Get();
+                timer = 0;
+            }
+        }
+    }
+
 }
+
